f186ca7ba9943798ffa2ca3345821f9c89869a3c
Tatsuaki Nemoto <tatsuaki.capricorni@gmail.com>
2021-01-27 00:44:13 +0900

    ikutsukano shuusei

--- plot_scatter.py
+++ plot_scatter.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data_set_x = np.loadtxt(
-    fname='/Users/nemomac2/Desktop/data310/raw/data_out310.csv', #読み込むファイルのパスと名前
+    fname='/home/abe/data/genome_data/data310/raw/data_out310.csv', #読み込むファイルのパスと名前
     dtype='float', #floatで読み込む
     delimiter=',', #csvなのでカンマで区切る
 )
 data_set_y = np.loadtxt(
-    fname='/Users/nemomac2/Desktop/data310/raw/data_test_out310.csv', #読み込むファイルのパスと名前
+    fname='/home/abe/data/genome_data/data310/raw/data_test_out310.csv', #読み込むファイルのパスと名前
     dtype='float', #floatで読み込む
     delimiter=',', #csvなのでカンマで区切る
 )
@@ -18,7 +18,9 @@ print(len(data_set_x[0]))
 print(len(data_set_y[0]))
 
 for i in range(len(data_set_x)):
+    print('data{}'.format(i))
     for j in range(len(data_set_x[0])):
+        print('data{}, {}'.format(i, j))
         plt.scatter(data_set_x[i][j], data_set_y[i][j])
 
 plt.title('test')
